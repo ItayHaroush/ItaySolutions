83483cd1ca561cb3113bf1f60372185ebfd1d8e2
ItayHaroush <itay666@icloud.com>
2026-08-02 15:21:37 +0300

    כגה

--- proposals/proposal-backend-config.js
+++ proposals/proposal-backend-config.js
@@ -5,3 +5,6 @@ window.PROPOSAL_SUBMIT_URL = "https://app.buildix.site/api/proposals/approve";
 window.PROPOSAL_PREPARE_URL = "https://app.buildix.site/api/proposals/prepare";
 window.PROPOSAL_BRANDING_URL = "https://app.buildix.site/api/proposals/branding";
 window.PROPOSAL_SUBMIT_API_KEY = "";
+window.PROPOSAL_TURNSTILE_CONFIG_URL = "https://app.buildix.site/api/public/turnstile/proposal_prepare";
+window.PROPOSAL_TURNSTILE_SITE_KEY = "0x4AAAAAADyOgFOmlKJsciFD";
+window.PROPOSAL_TURNSTILE_ACTION = "proposal_prepare";
